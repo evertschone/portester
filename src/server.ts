ff1ddd897a3bc092da0aa730b6c9478b47038ea6
Evert <evertschone@gmail.com>
2024-10-27 21:08:33 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001; // You can set this to any desired port
 
 app.get('/env', (req, res) => {
-    res.send(JSON.stringify(process.env));
+    res.send("ENV v1:"+JSON.stringify(process.env));
 });
 
 app.listen(port, () => {
